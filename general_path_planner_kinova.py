873443fdb3a98cc855e21f9ba81032d741b25615
Grimmlins <osu.grimmlins@gmail.com>
2022-03-18 21:17:48 +0000

    Ryan R: Reverted old version

--- general_path_planner_kinova.py
+++ general_path_planner_kinova.py
@@ -6,8 +6,6 @@
 # 
 # script for capturing/executing joint poses on kinova arm either virtually or in real world.
 #
-# Modified to benchmark Kinova arm
-#
 # referenced: kinova_path_planning.py by Nuha Nishat
 
 import rospy
@@ -50,13 +48,6 @@ class MoveRobot():
                 f.close()
             except Exception:
                 raise IOError("invalid csv file name. Cannot find csv file joint_angles directory")
-            # ADDED 2/24
-            # get in ee_goal positions
-            self.ee_goal_name = self.joint_angles_dir + csv_out
-            self.ee_goals = [] # list containing ee goals for path from csv_out
-            self.read_ee_goal()
-            # --
-
         #write joint angles
         elif(self.mode == 1):
             self.csv_name = self.joint_angles_dir + csv_out
@@ -126,13 +117,7 @@ class MoveRobot():
         self.move_group.allow_replanning(1)
 
         #how much to round joint angles
-        self.joint_angle_rounded = 2
-
-        # ADDED 2/24
-        # can change with set_goal_..._tolerance() ? - might be ky to use planned paths again!
-        print("jointT: {}".format(self.move_group.get_goal_joint_tolerance()))
-        print("poseT: {}".format(self.move_group.get_goal_position_tolerance()))
-        print("oreT: {}".format(self.move_group.get_goal_orientation_tolerance()))
+        self.joint_angle_rounded = 2 
 
     def set_planner_type(self, planner_name):
         if planner_name == "RRT":
@@ -145,7 +130,6 @@ class MoveRobot():
     def go_to_goal(self, ee_pose):
         """
         moves robot to current end-effector position
-
         Input: list of euler or quaternion values for end-effector 
         """
         pose_goal = Pose()
@@ -186,9 +170,7 @@ class MoveRobot():
     def go_to_finger_joint_state(self, joint_values):
         """
         moves finger joints to desired joint states.
-
         Input: list of joint values for each finger joint (list size of 3)
-
         Output: boolean stating whether path completion was successful (True) or not (False)
         """
         try:
@@ -206,13 +188,11 @@ class MoveRobot():
     def go_to_arm_joint_state(self, joint_values):
         """
         moves arm joints to desired joint states.
-
         Input: list of joint values for each arm joint (list size of 7) or name of pre-determined state
         
         current possible state names: (can add more in j2s7s300.srdf)
             - joint_values = "Home"
             - joint_values = "Vertical"
-
         Output: boolean stating whether path completion was successful (True) or not (False)
         """
         try:
@@ -260,26 +240,6 @@ class MoveRobot():
         for i in range(len(self.current_joint_values)):
             self.current_joint_values[i] = round(float(self.current_joint_values[i]), self.joint_angle_rounded)
 
-    # ADDED 2/24
-    def write_diff_list(self, diff_list):
-        diff_name =  self.csv_name[:len(self.csv_name) - 4] + "_joint_diff.csv"
-        with open(diff_name, mode="a") as f:
-            writer = csv.writer(f, delimiter=",", quotechar="|")
-            writer.writerow(diff_list)
-
-    # ADDED 2/24
-    def compute_ee_displacement(self, x, y, z):
-        return round(math.sqrt((x * x)+(y * y)+(z * z)),4)
-
-    # ADDED 2/24
-    def read_ee_goal(self):
-        with open(self.ee_goal_name, mode="r") as f:
-            reader = csv.reader(f, delimiter=" ", quotechar="|")
-            for row in reader:
-                parsed_row = row[0].split(",")
-                parsed_row = [float(v) for v in parsed_row]
-                self.ee_goals.append(copy.deepcopy(parsed_row))
-
     def write_joint_pose(self):
         """
         captures current joint values and writes to csv file specified by user.
@@ -317,30 +277,8 @@ class MoveRobot():
             #compare current and next joint lists. If same, don't set new goal, otherwise set and go to new joint goals
             if(not (functools.reduce(lambda x,y : x and y, map(lambda p,q : p == q, current_arm_joint_angles,next_arm_joint_angles), True))):
                 self.go_to_arm_joint_state(next_arm_joint_angles)
-            else:
-                print(" --- Arm joints skipped: already there!")
             if(not (functools.reduce(lambda x,y : x and y, map(lambda p,q : p == q, current_gripper_joint_angles,next_gripper_joint_angles), True))):
                 self.go_to_finger_joint_state(next_gripper_joint_angles)
-            else:
-                print(" --- Gripper joints skipped: already there!")
-            # ADDED 2/24, compare resulting position of real arm --
-            test = self.move_group.get_current_pose() # current end effector position
-            # # write out ee goals
-            # tmp = [test.pose.position.x, test.pose.position.y, test.pose.position.z]
-            # self.write_diff_list(tmp)
-
-            # compute ee differences
-            print(self.compute_ee_displacement(test.pose.position.x - self.ee_goals[i][0], test.pose.position.y - self.ee_goals[i][1], test.pose.position.z - self.ee_goals[i][2]))
-
-            # self.capture_joint_pose()
-            # tmp = []
-            # for x in range(len(self.current_joint_values)):
-            #     # actual - goal
-            #     tmp.append(round(float(self.current_joint_values[x] - self.joint_poses[i][x]), self.joint_angle_rounded))
-            # self.write_diff_list(tmp)
-            # # --
-
-        # Done with csv file, clear joint_poses list
         self.joint_poses = []
 
     def Run(self):
@@ -377,23 +315,19 @@ if __name__ == '__main__':
     First arg: mode 
         - read joint poses (0)
         - capture joint poses (1)
-
     Second arg: environment
         - Real robot launch (0)
         - Virtual robot launch (1)
         - redirected ROS topic (i.e. gazebo launch) (2)
-
     read joint poses:
         third arg: 
             - name of csv file containing joint poses
-
     capture joint poses:
         third arg: 
             - move robot (in rviz or real world w/ controller) and capture joint angles on command (0)
             - run custom code (1)
         fourth arg:
             - name of csv file to be written to
-
     Notes:
         - gripper joint values will return as 0.0 until they are actually moved for the first time
         - all joint angle files are read/written from the joint_angles folder
